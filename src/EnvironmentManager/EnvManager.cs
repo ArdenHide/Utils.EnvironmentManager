28267406b56ed625afc8a1c235cbe13f06dd4d1d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-06-03 12:22:56 +0300

    - fix build error (#48)

--- src/EnvironmentManager/EnvManager.cs
+++ src/EnvironmentManager/EnvManager.cs
@@ -1,65 +1,67 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
-namespace EnvironmentManager;
-
-public class EnvManager
+namespace EnvironmentManager
 {
-    private readonly IMapper mapper;
-    private readonly ILogger<EnvManager> logger;
-
-    public EnvManager(ILogger<EnvManager>? logger = null)
-        : this(new EnvManagerMappingConfigurator().Build(), logger)
-    { }
-
-    public EnvManager(IConfigurationProvider config, ILogger<EnvManager>? logger = null)
+    public class EnvManager
     {
-        mapper = new Mapper(config);
-        this.logger = logger ?? NullLogger<EnvManager>.Instance;
-    }
+        private readonly IMapper mapper;
+        private readonly ILogger<EnvManager> logger;
 
-    public object GetEnvironmentValue(Type type, string variableName, bool raiseException = false)
-    {
-        var method = typeof(EnvManager).GetMethod(nameof(GetEnvironmentValue), new[] { typeof(string), typeof(bool) });
-        var genericMethod = method!.MakeGenericMethod(type);
-        return genericMethod.Invoke(this, new object[] { variableName, raiseException })!;
-    }
+        public EnvManager(ILogger<EnvManager>? logger = null)
+            : this(new EnvManagerMappingConfigurator().Build(), logger)
+        { }
 
-    public T GetEnvironmentValue<T>(string variableName, bool raiseException = false)
-    {
-        var envValue = Environment.GetEnvironmentVariable(variableName);
-
-        if (!string.IsNullOrEmpty(envValue))
+        public EnvManager(IConfigurationProvider config, ILogger<EnvManager>? logger = null)
         {
-            return ConvertEnvironmentValue<T>(variableName, envValue, raiseException);
+            mapper = new Mapper(config);
+            this.logger = logger ?? NullLogger<EnvManager>.Instance;
         }
 
-        if (raiseException)
+        public object GetEnvironmentValue(Type type, string variableName, bool raiseException = false)
         {
-            throw new InvalidOperationException($"Environment variable '{variableName}' is null or empty.");
+            var method = typeof(EnvManager).GetMethod(nameof(GetEnvironmentValue), new[] { typeof(string), typeof(bool) });
+            var genericMethod = method!.MakeGenericMethod(type);
+            return genericMethod.Invoke(this, new object[] { variableName, raiseException })!;
         }
 
-        logger.LogWarning("Environment variable '{VariableName}' is null or empty.", variableName);
-
-        return default!;
-    }
-
-    private T ConvertEnvironmentValue<T>(string variableName, string envValue, bool raiseException)
-    {
-        try
+        public T GetEnvironmentValue<T>(string variableName, bool raiseException = false)
         {
-            return mapper.Map<string, T>(envValue);
-        }
-        catch (Exception ex)
-        {
-            if (raiseException)
+            var envValue = Environment.GetEnvironmentVariable(variableName);
+
+            if (!string.IsNullOrEmpty(envValue))
             {
-                throw new InvalidCastException($"Failed to convert environment variable '{variableName}' to type '{typeof(T)}'.", ex);
+                return ConvertEnvironmentValue<T>(variableName, envValue, raiseException);
             }
 
-            logger.LogError("Failed to convert environment variable '{VariableName}' to type '{Type}'. Returning default value.", variableName, typeof(T));
+            if (raiseException)
+            {
+                throw new InvalidOperationException($"Environment variable '{variableName}' is null or empty.");
+            }
+
+            logger.LogWarning("Environment variable '{VariableName}' is null or empty.", variableName);
+
             return default!;
+        }
+
+        private T ConvertEnvironmentValue<T>(string variableName, string envValue, bool raiseException)
+        {
+            try
+            {
+                return mapper.Map<string, T>(envValue);
+            }
+            catch (Exception ex)
+            {
+                if (raiseException)
+                {
+                    throw new InvalidCastException($"Failed to convert environment variable '{variableName}' to type '{typeof(T)}'.", ex);
+                }
+
+                logger.LogError("Failed to convert environment variable '{VariableName}' to type '{Type}'. Returning default value.", variableName, typeof(T));
+                return default!;
+            }
         }
     }
 }
