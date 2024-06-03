3dda7bb573abbf52aee0ff210ead1d65b46798a3
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-06-03 08:30:44 +0300

    Create a dynamic tool for working with enum as a variables names (#46)
    * Create a dynamic tool for working with enum as a variables names
    
    * - add test
    
    * - cleanup

---
+++ src/EnvironmentManager/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace EnvironmentManager.Extensions;
+
+public static class EnumExtensions
+{
+    private static readonly EnvManager envManager = new();
+
+    public static TResponse GetEnvironmentValue<TEnum, TResponse>(this TEnum key, bool raiseException = false)
+        where TEnum : Enum
+    {
+        return envManager.GetEnvironmentValue<TResponse>(key.ToString(), raiseException);
+    }
+}
