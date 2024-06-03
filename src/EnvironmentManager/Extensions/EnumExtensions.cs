5834a3f7567658857e630f9a8bfb033e19d5f8ce
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-03 08:29:55 +0300

    - add test

--- src/EnvironmentManager/Extensions/EnumExtensions.cs
+++ src/EnvironmentManager/Extensions/EnumExtensions.cs
@@ -4,7 +4,7 @@ public static class EnumExtensions
 {
     private static readonly EnvManager envManager = new();
 
-    public static TResponse Get<TEnum, TResponse>(this TEnum key, bool raiseException = false)
+    public static TResponse GetEnvironmentValue<TEnum, TResponse>(this TEnum key, bool raiseException = false)
         where TEnum : Enum
     {
         return envManager.GetEnvironmentValue<TResponse>(key.ToString(), raiseException);
---
+++ test/EnvironmentManager.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using EnvironmentManager.Extensions;
+
+namespace EnvironmentManager.Tests.Extensions;
+
+public class EnumExtensionsTests
+{
+    internal enum Animal { DOG, CAT, COW }
+
+    internal static IDictionary<Animal, string> Sounds => new Dictionary<Animal, string>
+    {
+        { Animal.DOG, "woof woof" },
+        { Animal.CAT, "meow meow" },
+        { Animal.COW, "moo" }
+    };
+
+    public class GetEnvironmentValue
+    {
+        public GetEnvironmentValue()
+        {
+            Sounds.ToList().ForEach(x => Environment.SetEnvironmentVariable(x.Key.ToString(), x.Value));
+        }
+
+        [Theory]
+        [InlineData(Animal.DOG)]
+        [InlineData(Animal.CAT)]
+        [InlineData(Animal.COW)]
+        internal void WhenValueExist_WhenRaiseExceptionSetFalse(Animal animal)
+        {
+            var value = animal.GetEnvironmentValue<Animal, string>(false);
+
+            value.Should().Be(Sounds[animal]);
+        }
+    }
+}
