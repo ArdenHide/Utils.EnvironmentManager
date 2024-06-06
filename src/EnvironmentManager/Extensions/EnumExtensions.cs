60e811b3d4b6b017535b54e87fe8a5ea2f7bad78
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-06 22:29:28 +0300

    Update `EnumExtensions`: Provide a better user interface

--- src/EnvironmentManager/Extensions/EnumExtensions.cs
+++ src/EnvironmentManager/Extensions/EnumExtensions.cs
@@ -4,12 +4,17 @@ namespace EnvironmentManager.Extensions
 {
     public static class EnumExtensions
     {
-        private static readonly EnvManager envManager = new EnvManager();
+        private static readonly EnvManager _envManager = new EnvManager();
 
-        public static TResponse GetEnvironmentValue<TEnum, TResponse>(this TEnum key, bool raiseException = false)
-            where TEnum : Enum
+        public static TResponse GetEnvironmentValue<TResponse>(this Enum key, bool raiseException = false, EnvManager? envManager = null)
         {
+            envManager ??= _envManager;
             return envManager.GetEnvironmentValue<TResponse>(key.ToString(), raiseException);
+        }
+
+        public static string GetEnvironmentValue(this Enum key, bool raiseException = false, EnvManager? envManager = null)
+        {
+            return key.GetEnvironmentValue<string>(raiseException, envManager);
         }
     }
 }
