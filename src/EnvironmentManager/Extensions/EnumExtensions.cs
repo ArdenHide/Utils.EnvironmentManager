942ae69fdc6fb2341aabf6dd0d1c3473ad88d2e1
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-06-08 08:42:33 +0300

    Create custom attribute for `EnumExtensions` (#56)
    * Create custom attribute for `EnumExtensions`
    
    * - update XML
    - update tests

--- src/EnvironmentManager/Extensions/EnumExtensions.cs
+++ src/EnvironmentManager/Extensions/EnumExtensions.cs
@@ -1,38 +1,33 @@
 ﻿using System;
+using System.Reflection;
+using EnvironmentManager.Attributes;
 
 namespace EnvironmentManager.Extensions
 {
     /// <summary>
-    /// Provides extension methods for Enums to work with environment settings.
+    /// Provides extension methods for Enums to work with environment settings, using attributes to specify type and requirement status.
     /// </summary>
     public static class EnumExtensions
     {
         private static readonly EnvManager _envManager = new EnvManager();
 
         /// <summary>
-        /// Retrieves an environment value associated with the specified enum key.
+        /// Retrieves an environment value associated with the specified enum key.<br/>
+        /// Utilizes <see cref="EnvironmentVariableAttribute"/> to determine the type and requirement of the environment variable.<br/>
+        /// If no attribute is found, defaults to a string type and not required.
         /// </summary>
-        /// <typeparam name="TResponse">The type of the environment value to retrieve.</typeparam>
         /// <param name="key">The enum key associated with the environment value.</param>
-        /// <param name="raiseException">Specifies whether to throw an exception if the environment value is not found.</param>
         /// <param name="envManager">Optional. An instance of <see cref="EnvManager"/> to use for retrieving environment values. If not provided, a default instance is used.</param>
-        /// <returns>The environment value cast to the specified type.</returns>
-        public static TResponse GetEnvironmentValue<TResponse>(this Enum key, bool raiseException = false, EnvManager? envManager = null)
+        /// <returns>The environment value cast to the type specified in the <see cref="EnvironmentVariableAttribute"/>, or to string if no attribute is set.</returns>
+        public static object GetEnvironmentValue(this Enum key, EnvManager? envManager = null)
         {
-            envManager ??= _envManager;
-            return envManager.GetEnvironmentValue<TResponse>(key.ToString(), raiseException);
-        }
+            var attribute = key.GetType().GetField(key.ToString())?.GetCustomAttribute<EnvironmentVariableAttribute>();
 
-        /// <summary>
-        /// Retrieves a string environment value associated with the specified enum key.
-        /// </summary>
-        /// <param name="key">The enum key associated with the environment value.</param>
-        /// <param name="raiseException">Specifies whether to throw an exception if the environment value is not found.</param>
-        /// <param name="envManager">Optional. An instance of <see cref="EnvManager"/> to use for retrieving environment values. If not provided, a default instance is used.</param>
-        /// <returns>The environment value as a string.</returns>
-        public static string GetEnvironmentValue(this Enum key, bool raiseException = false, EnvManager? envManager = null)
-        {
-            return key.GetEnvironmentValue<string>(raiseException, envManager);
+            var targetType = attribute?.Type ?? typeof(string);
+            var raiseException = attribute?.IsRequired ?? false;
+
+            envManager ??= _envManager;
+            return envManager.GetEnvironmentValue(targetType, key.ToString(), raiseException);
         }
     }
 }
